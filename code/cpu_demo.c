26f8bf6f8e1a7132f59a3279e87447e4ddff3fec
Robert D. French <robert@robertdfrench.me>
2024-07-20 13:34:51 -0400

    Describe how stub functions are resolved by linker

--- code/cpu_demo.c
+++ code/cpu_demo.c
@@ -1,3 +1,15 @@
+// This program shows a simple example of using gnu ifunc. The idea is to create
+// a "stub" function called "print_cpu_info". Whenever this stub function is
+// called at runtime, it will point to one of the following real functions:
+// 
+// * print_avx2
+// * print_sse42
+// * print_cpu_sucks
+//
+// The decision about which of the real functions to call is made by the
+// "resolve_cpu_info" function. That function is called by the dynamic linker as
+// the program is being loaded loaded into memory, before "main" even starts.
+//
 // This is a simplified version of the code presented at
 // https://jasoncc.github.io/gnu_gcc_glibc/gnu-ifunc.html.
 #include <stdio.h>
@@ -24,11 +36,16 @@ void print_cpu_sucks() {
 // resolver function only has to be run once.
 void print_cpu_info() __attribute__((ifunc ("resolve_cpu_info")));
 
-// This is the "resolver" function. The first time the program calls
-// print_cpu_info(), it will call this function to determine which function
-// really needs to be run.
+// This is the "resolver" function. The dynamic linker will call this function
+// when the program is being loaded in order to determine what function should
+// actually be called when the program calls the "print_cpu_info" stub.
 static void* resolve_cpu_info(void) {
+
+        // This is to show that the resolver runs before main.
 	printf("Running the Resolver\n");
+
+        // Here we check for CPU features to determine which function should be
+        // run when the program calls the stub function "print_cpu_info".
 	if (__builtin_cpu_supports("avx2")) {
 		return print_avx2;
 	} else if (__builtin_cpu_supports("sse4.2")) {
@@ -39,7 +56,11 @@ static void* resolve_cpu_info(void) {
 }
 
 int main() {
+        // Show that the resolver runs before main.
         printf("Entering main\n");
+
+        // Call whatever function that "resolve_cpu_info" has decided that we
+        // should use here.
 	print_cpu_info();
 	return 0;
 }
